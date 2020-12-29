7cda979e1c6649260d75bd3ee8e7875a3a4905f7
郑肖肖 <zhengxiaoxiao@m-clinical.cn>
2020-12-29 18:08:12 +0800

    feat: react-router

---
+++ src/reactRouter/RouterDom.js
@@ -0,0 +1,10 @@
+
+// react-router-dom: 基于react-router，加入了在浏览器运行环境下的一些功能：
+
+// 路由器： <BrowserRouter></BrowserRouter>, <HashRouter></HashRouter>
+// 路由匹配器：<Router> <Switch>
+// 导航： <Link> <NavLink>  <Redirect>
+
+// Link组件，会渲染一个a标签；
+// BrowserRouter组件，使用pushState和popState事件构建路由；
+// HashRouter组件，使用window.location.hash和hashchange事件构建路由。
