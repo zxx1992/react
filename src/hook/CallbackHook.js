c8cf6f1c56c82022519bc2bc7b0fecfcc614b7e0
郑肖肖 <zhengxiaoxiao@m-clinical.cn>
2021-01-07 15:09:12 +0800

    feat: 不要过度使用React.useCallback()  实例

--- src/hook/CallbackHook.js
+++ src/hook/CallbackHook.js
@@ -35,39 +35,90 @@ import React, { memo, useCallback, useState } from 'react'
 //   const { onClick, children } = props;
 //   return <div onClick={onClick}>{children}</div>
 // }
-const PageA = memo((props) => {
-  console.log(props, "PageA")
-  const { onClick, children } = props;
-  return <div onClick={onClick}>{children}</div>
-})
+// const PageA = memo((props) => {
+//   console.log(props, "PageA")
+//   const { onClick, children } = props;
+//   return <div onClick={onClick}>{children}</div>
+// })
 // function PageB(props) {
 //   console.log(props,"props.name")
 //   return <div onClick={props.onClick}>{props.name}</div>
 // }
 // 使用memo包裹PageB组件
-const PageB = memo((props) => {
-  console.log(props, "PageB")
-  return <div onClick={props.onClick}>{props.name}</div>
-})
-function CallbackHook() {
-  const [a, setA] = useState(0);
-  const [b, setB] = useState(0);
-  const handleClick1 = useCallback(() => {
-      setA(a + 1)
-    }, [a])
-  const handleClick2 = useCallback(() => {
-    setB(b + 1)
-  }, [b])
+// const PageB = memo((props) => {
+//   console.log(props, "PageB")
+//   return <div onClick={props.onClick}>{props.name}</div>
+// })
+// function CallbackHook() {
+//   const [a, setA] = useState(0);
+//   const [b, setB] = useState(0);
+//   const handleClick1 = useCallback(() => {
+//     setA(a + 1)
+//   }, [a])
+//   const handleClick2 = useCallback(() => {
+//     setB(b + 1)
+//   }, [b])
 
+//   return (
+//     <div>
+//       <PageA onClick={handleClick1}>{a}</PageA>
+//       <PageB onClick={handleClick2} name={b}></PageB>
+//     </div>
+//   )
+// }
+
+
+
+// 不要过度使用React.useCallback()，应该是子组件非常耗时的时候，才考虑缓存回调函数；
+// 这里子组件渲染了一个项目列表，列表很大，有几百个项目，这市就可以考虑使用memo封装，
+const MyBigList = memo((props) => {
+  const { items, handleClick } = props;
+  console.log(handleClick,"222")
+  const lists = items.map((item, index) => {
+    return (<li key={index} onClick={() => handleClick()}>{item.name}</li>)
+  })
+  return <div>{lists}</div>
+})
+
+
+// 这里说只要term不变，useCallback就会返回相同的函数对象
+// handleClick只和term有关(还是不太懂，是否用useCallback包裹，好像都一样)
+function CallbackHook() {
+  const [term, setTerm] = useState("term")
+  const handleClick = useCallback((item) => {
+    console.log('you click', item)
+  }, [term]);
+  // const handleClick = (item) => {
+  //   console.log('you click', item)
+  // };
+  const items = [{
+    name: "1"
+  }, {
+    name: "1"
+  }, {
+    name: "1"
+  }, {
+    name: "1"
+  }, {
+    name: "1"
+  }, {
+    name: "1"
+  }, {
+    name: "1"
+  }, {
+    name: "1"
+  }, {
+    name: "1"
+  }, {
+    name: "1"
+  },]
   return (
     <div>
-      <PageA onClick={handleClick1}>{a}</PageA>
-      <PageB onClick={handleClick2} name={b}></PageB>
+      {term}
+      <button onClick={() => setTerm("term123")}>changeTerm</button>
+      <MyBigList items={items} handleClick={handleClick}></MyBigList>
     </div>
   )
 }
 
 export default CallbackHook;
-
-
-// 不要过度使用React.useCallback()，应该是子组件非常耗时的时候，才考虑缓存回调函数；
