b2276711f1029182dcd0f62ebc11ca0f7d503d5b
郑肖肖 <zhengxiaoxiao@m-clinical.cn>
2021-01-07 14:03:09 +0800

    feat: useCallback使用场景

--- src/hook/CallbackHook.js
+++ src/hook/CallbackHook.js
@@ -1,15 +1,69 @@
-import React, { useCallback } from 'react'
+import React, { memo, useCallback, useState } from 'react'
+
+// const CallbackHook = memo(() => {
+//   const [currentVal, setCurrentVal] = useState("");
+
+//   const memoizedCallback = useCallback(
+//     (index) => {
+//       // if (currentVal !== index) {
+//         console.log(index,"1")
+//         setCurrentVal(index)
+//       // }
+//     },
+//     [currentVal],
+//   );
+
+//   // const memoizedCallback = (index) => {
+//   //   console.log(index,"1")
+//   //   setCurrentVal(index)
+//   // }
+//   return (
+//     <div>
+//       {currentVal}
+//       <button onClick={() => memoizedCallback("")}>change</button>
+//     </div>
+//   )
+// })
 
 
+// https://zhuanlan.zhihu.com/p/56975681  React.memo 和 React.useCallback 一定记得需要配对使用，缺了一个都可能导致性能不升反“降”，毕竟无意义的浅比较也是要消耗那么一点点点的性能。
+// 
+
+// 这里没有使用useCallback, 点击子组件1，子组件2也会重新渲染；相反也是一样的；
+// function PageA(props) {
+//   console.log(props, "PageA")
+//   const { onClick, children } = props;
+//   return <div onClick={onClick}>{children}</div>
+// }
+const PageA = memo((props) => {
+  console.log(props, "PageA")
+  const { onClick, children } = props;
+  return <div onClick={onClick}>{children}</div>
+})
+// function PageB(props) {
+//   console.log(props,"props.name")
+//   return <div onClick={props.onClick}>{props.name}</div>
+// }
+// 使用memo包裹PageB组件
+const PageB = memo((props) => {
+  console.log(props, "PageB")
+  return <div onClick={props.onClick}>{props.name}</div>
+})
 function CallbackHook() {
-  const memoizedCallback = useCallback(
-    () => {
-      console.log("wwww")
-    },
-    [1, 2],
-  );
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+  const handleClick1 = useCallback(() => {
+      setA(a + 1)
+    }, [a])
+  const handleClick2 = useCallback(() => {
+    setB(b + 1)
+  }, [b])
+
   return (
-    <div>callback</div>
+    <div>
+      <PageA onClick={handleClick1}>{a}</PageA>
+      <PageB onClick={handleClick2} name={b}></PageB>
+    </div>
   )
 }
 
